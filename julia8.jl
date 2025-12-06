a746bb2705fe83b90b4b50475f14525a494f7846
GoatMashine <goat@Laptop-von-goat.local>
2025-12-06 22:26:23 +0100

    implemented some functions

--- julia8.jl
+++ julia8.jl
@@ -25,51 +25,101 @@ font = [
     0xF0, 0x80, 0xF0, 0x80, 0x80  # F
 ]
 
+# load font into memory
 for i in eachindex(font)
     mem[0x50 + i] = font[i]
 end    
 
 pc = 0x201
 
+# functions
+
+function load(x::UIn8, y::UInt8)
+    opcode = (x << 8) | y
+    pc += 2
+    decode(opcode)
+end
+
+function decode(opcode::UInt16)
+    nib1 = UInt8((opcode >> 12) & 0xF)
+    if nib1 == 0x0
+        if opcode == 0x00E0
+            clear_screen()
+        elseif opcode == 0x00EE
+            
+        else
+            return
+        end        
+
+    elseif nib1 == 0x1
+        NNN = UInt16(opcode & 0x0FFF)
+        jump(NNN)
+    elseif nib1 == 0x2
+
+    elseif nib1 == 0x3
+
+    elseif nib1 == 0x4
+
+    elseif nib1 == 0x5
+
+    elseif nib1 == 0x6
+        XNN = UInt16(opcode & 0x0FFF)
+        set_register(XNN)
+    elseif nib1 == 0x7
+        XNN = UInt16(opcode & 0x0FFF)
+        add_register(XNN)
+    elseif nib1 == 0x8
+
+    elseif nib1 == 0x9
+
+    elseif nib1 == 0xA
+        NNN = UInt16(opcode & 0x0FFF)
+        set_I(NNN)
+    elseif nib1 == 0xB
+
+    elseif nib1 == 0xC
+
+    elseif nib1 == 0xD
+
+    elseif nib1 == 0xE
+
+    elseif nib1 == 0xF
+
+    end
+end
+
+function
+
+function jump(NNN::UInt16) # 1NNN
+    pc = NNN
+    return
+end
+
+function set_register(XNN::UInt16) # 6XNN
+    X = UInt8(XNN >> 8) & 0xF
+    NN = UInt8(XNN & 0xFF)
+    v[X + 1] = NN
+    return
+end    
+
+function add_register(XNN::UInt16) # 7XNN
+    X = UInt8(XNN >> 8) & 0xF
+    NN = UInt8(XNN & 0xFF)
+    v[X + 1] += NN
+    return
+end    
+
+function set_I(NNN::UInt16) # ANNN
+    I = NNN
+end    
+
+function clear_screen() # 00E0
+    screen = falses(32, 64)
+    return
+end    
+
 
 # main F/D/E Loop
 while true
-    opcode = (UInt16(mem[pc]) << 8) | mem[pc + 1]
-    nib1 = UInt8((opcode >> 12) & 0xF)
-    nib2 = UInt8((opcode >> 8) & 0xF)
-    nib3 = UInt8((opcode >> 4) & 0xF)
-    nib4 = UInt8(opcode & 0xF)
-    if nib1 == 0x0
-        print(0)
-    elseif nib1 == 0x1
-        print(1)
-    elseif nib1 == 0x2
-        print(2)
-    elseif nib1 == 0x3
-        print(3)
-    elseif nib1 == 0x4
-        print(4)
-    elseif nib1 == 0x5
-        print(5)
-    elseif nib1 == 0x6
-        print(6)
-    elseif nib1 == 0x7
-        print(7)
-    elseif nib1 == 0x8
-        print(8)
-    elseif nib1 == 0x9
-        print(9)
-    elseif nib1 == 0xA
-        print("A")
-    elseif nib1 == 0xB
-        print(0xB)
-    elseif nib1 == 0xC
-        print(0xC)
-    elseif nib1 == 0xD
-        print(0xD)
-    elseif nib1 == 0xE
-        print(0xE)
-    elseif nib1 == 0xF
-        print(0xF)   
-    end    
+    load(mem[pc + 1], mem[pc + 2]) 
 end    
